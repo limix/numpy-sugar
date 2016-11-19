14f25feceb70dc394b14814104c4886b8f3efa0c
Danilo Horta <danilo.horta@gmail.com>
2016-11-19 15:50:53 +0000

    tri

--- numpy_sugar/linalg/tri.py
+++ numpy_sugar/linalg/tri.py
@@ -1,13 +1,21 @@
 from scipy.linalg import solve_triangular
 
 
-def stl(a, b):
-    return solve_triangular(a, b, lower=True, check_finite=False)
+def stl(A, b):
+    r"""Shortcut to ``solve_triangular(A, b, lower=True, check_finite=False)``.
 
+    Solve linear systems :math:`\mathrm A \mathbf x = \mathbf b` when
+    :math:`\mathrm A` is a lower-triangular matrix.
 
-def stu(a, b):
-    return solve_triangular(a, b, lower=False, check_finite=False)
+    Args:
+        A (array_like): A lower-triangular matrix.
+        b (array_like): Ordinate values.
 
+    Returns:
+        :class:`numpy.ndarray`: Solution ``x``.
 
-def tri_solve(L, x):
-    return solve_triangular(L, x, lower=True, check_finite=False)
+    See Also
+    --------
+    scipy.linalg.solve_triangular
+    """
+    return solve_triangular(A, b, lower=True, check_finite=False)
