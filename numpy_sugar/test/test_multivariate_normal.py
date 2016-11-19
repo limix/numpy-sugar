9b2dd35baf005859aff5fb0f0ef07d57d5c28c45
Danilo Horta <danilo.horta@gmail.com>
2016-11-19 09:47:20 +0000

    12 tests passed

--- numpy_sugar/test/test_multivariate_normal.py
+++ numpy_sugar/test/test_multivariate_normal.py
@@ -1,27 +1,27 @@
-# from __future__ import division
-#
-# from numpy import dot
-# from numpy.random import RandomState
-# from numpy.testing import assert_allclose
-#
-# from limix_math import multivariate_normal
-#
-# def test_multivariate_normal():
-#     rs = RandomState(3)
-#     m = rs.randn(10)
-#     G = rs.randn(10, 12)
-#     K = dot(G, G.T)
-#     assert_allclose(multivariate_normal(m, K, rs), [7.67701071054,
-#         1.36892007698,
-#         -6.43324011594,
-#         -7.34236124724,
-#         -1.27151910971,
-#         3.19037731625,
-#         0.90263960298,
-#         -1.85267387027,
-#         -0.735702280982,
-#         0.948785481537])
-#
-#
-# if __name__ == '__main__':
-#     __import__('pytest').main([__file__, '-s'])
+from __future__ import division
+
+from numpy import dot
+from numpy.random import RandomState
+from numpy.testing import assert_allclose
+
+from numpy_sugar.random import multivariate_normal
+
+def test_multivariate_normal():
+    rs = RandomState(3)
+    m = rs.randn(10)
+    G = rs.randn(10, 12)
+    K = dot(G, G.T)
+    assert_allclose(multivariate_normal(m, K, rs), [7.67701071054,
+        1.36892007698,
+        -6.43324011594,
+        -7.34236124724,
+        -1.27151910971,
+        3.19037731625,
+        0.90263960298,
+        -1.85267387027,
+        -0.735702280982,
+        0.948785481537])
+
+
+if __name__ == '__main__':
+    __import__('pytest').main([__file__, '-s'])
