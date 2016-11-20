c88fd52c5a445c6cfa164a0ae4f119ec35cbacb2
Danilo Horta <danilo.horta@gmail.com>
2016-11-20 11:00:39 +0000

    do not include 'm' in msvs

--- special_build.py
+++ special_build.py
@@ -13,13 +13,19 @@ def _make():
     hdr = join('numpy_sugar', 'include', 'numpy_sugar', 'special.h')
     incls = [join('numpy_sugar', 'include'), get_include()]
 
+    from distutils.ccompiler import new_compiler
+    compiler = new_compiler()
+    libraries = ['ncprob', 'nmisc']
+    if 'msv' not in compiler.__class__.__name__.lower():
+        libraries += ['m']
+
     ffi.set_source(
         'numpy_sugar.special._special_ffi',
         '#include "numpy_sugar/special.h"',
         include_dirs=incls,
         sources=sources,
         extra_compile_args=['-Ofast'],
-        libraries=['ncprob', 'nmisc', 'm'],
+        libraries=libraries,
         library_dirs=[get_lib()])
 
     with open(hdr, 'r') as f:
