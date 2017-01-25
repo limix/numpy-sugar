95fc760c961dc860ea9de4c7ef845d136c8ffc36
Danilo Horta <danilo.horta@gmail.com>
2017-01-25 09:31:46 +0000

    38 again

--- travis/install_clang39.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo add-apt-repository -y 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.9 main' || true
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add - || true
-sudo apt-get update -y || true
-sudo apt-get install -qq -y clang-3.9 zlib1g-dev libbz2-dev || true
