d5bfd6b77125990b0005329488b5daef91e3b4c9
Danilo Horta <danilo.horta@gmail.com>
2017-09-28 11:09:55 +0100

    stage no special

--- doc/index.rst
+++ doc/index.rst
@@ -12,7 +12,6 @@ Missing NumPy functionalities.
     install
     array
     algebra
-    special
 
 *****************
 Comments and bugs
--- numpy_sugar/__init__.py
+++ numpy_sugar/__init__.py
@@ -10,7 +10,7 @@ from __future__ import absolute_import as _
 
 import _cffi_backend as _
 
-from . import epsilon, linalg, ma, random, special
+from . import epsilon, linalg, ma, random
 from ._array import cartesian, is_all_equal, is_all_finite, is_crescent, unique
 from ._test import test
 from .api import get_include, get_lib
@@ -22,7 +22,6 @@ __author_email__ = "horta@ebi.ac.uk"
 
 __all__ = [
     "__name__", "__version__", "__author__", "__author_email__", "test",
-    "epsilon", "linalg", "ma", "random", "special", "cartesian",
-    "is_all_equal", "is_all_finite", "is_crescent", "unique", "get_include",
-    "get_lib"
+    "epsilon", "linalg", "ma", "random", "cartesian", "is_all_equal",
+    "is_all_finite", "is_crescent", "unique", "get_include", "get_lib"
 ]
