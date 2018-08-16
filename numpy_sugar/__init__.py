ac5c31e9780ebdbd2610fcadec55eb3922c18b22
Danilo Horta <danilo.horta@gmail.com>
2018-08-16 18:24:25 +0100

    zip_safe=False

--- numpy_sugar/__init__.py
+++ numpy_sugar/__init__.py
@@ -12,7 +12,7 @@ from . import epsilon, linalg, ma, random
 from ._array import cartesian, is_all_equal, is_all_finite, is_crescent, unique
 from ._testit import test
 
-__version__ = "1.2.7"
+__version__ = "1.2.8"
 
 __all__ = [
     "__version__",
