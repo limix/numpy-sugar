804e66b251fa19832d25bb2a19528061341ba6bc
Danilo Horta <danilo.horta@pm.me>
2020-06-08 13:37:40 +0100

    trigger commit

--- doc/conf.py
+++ doc/conf.py
@@ -52,7 +52,6 @@ todo_include_todos = False
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 html_sidebars = {"**": ["relations.html", "searchbox.html"]}
 
 htmlhelp_basename = "{}doc".format(project)
