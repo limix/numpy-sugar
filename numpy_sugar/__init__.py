f9854bfa71520b062b78232a4053230189133a06
Danilo Horta <danilo.horta@pm.me>
2021-09-14 22:40:17 +0100

    dev1

--- numpy_sugar/__init__.py
+++ numpy_sugar/__init__.py
@@ -12,7 +12,7 @@ from ._rankdata import nanrankdata
 from ._testit import test
 from .linalg import ddot, dotd
 
-__version__ = "1.5.2"
+__version__ = "1.5.3.dev1"
 
 __all__ = [
     "__version__",
